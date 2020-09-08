e9035cdbe5801d4b33129b0b5fc9f6b9f7f72577
Vasyl <vasultm@yahoo.com>
2020-09-08 10:19:06 +0300

    Initial Commit

---
+++ Recipe Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipe Search
+//
+//  Created by Vasyl Travlinskyy on 08.09.2020.
+//  Copyright © 2020 Vasyl Travlinskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
